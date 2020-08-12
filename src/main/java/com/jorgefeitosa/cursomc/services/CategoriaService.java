3a0cb4b5aa940cd99c4d210360433a67aacfea5e
jorge Feitosa <jorgefeitosa.btu@gmail.com>
2020-08-11 23:11:50 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/jorgefeitosa/cursomc/services/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.jorgefeitosa.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jorgefeitosa.cursomc.domain.Categoria;
+import com.jorgefeitosa.cursomc.repositories.CategoriaRepository;
+
+
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		 Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		} 
+
+}
