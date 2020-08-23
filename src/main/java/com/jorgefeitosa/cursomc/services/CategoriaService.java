8a0c5bece9c0647f0eb465e557245d45b174a954
jorge Feitosa <jorgefeitosa.btu@gmail.com>
2020-08-23 11:56:38 -0300

    Ajustes no endPpint /categorias/ {id}

--- src/main/java/com/jorgefeitosa/cursomc/services/CategoriaService.java
+++ src/main/java/com/jorgefeitosa/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.jorgefeitosa.cursomc.domain.Categoria;
 import com.jorgefeitosa.cursomc.repositories.CategoriaRepository;
+import com.jorgefeitosa.cursomc.services.exception.ObjectNotFoundException;
 
 
 
@@ -16,9 +17,10 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		 Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 		} 
 
 }
