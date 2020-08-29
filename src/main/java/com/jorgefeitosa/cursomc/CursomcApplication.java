35ebdb7a5c66028847bdebb0e08aaf37eced8863
jorge Feitosa <jorgefeitosa.btu@gmail.com>
2020-08-29 19:36:12 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/jorgefeitosa/cursomc/CursomcApplication.java
+++ src/main/java/com/jorgefeitosa/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.jorgefeitosa.cursomc.domain.Cidade;
 import com.jorgefeitosa.cursomc.domain.Cliente;
 import com.jorgefeitosa.cursomc.domain.Endereco;
 import com.jorgefeitosa.cursomc.domain.Estado;
+import com.jorgefeitosa.cursomc.domain.ItemPedido;
 import com.jorgefeitosa.cursomc.domain.Pagamento;
 import com.jorgefeitosa.cursomc.domain.PagamentoComBoleto;
 import com.jorgefeitosa.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.jorgefeitosa.cursomc.repositories.CidadeRepository;
 import com.jorgefeitosa.cursomc.repositories.ClienteRepository;
 import com.jorgefeitosa.cursomc.repositories.EnderecoRepository;
 import com.jorgefeitosa.cursomc.repositories.EstadoRepository;
+import com.jorgefeitosa.cursomc.repositories.ItemPedidoRepository;
 import com.jorgefeitosa.cursomc.repositories.PagamentoRepository;
 import com.jorgefeitosa.cursomc.repositories.PedidoRepository;
 import com.jorgefeitosa.cursomc.repositories.ProdutoRepository;
@@ -48,7 +50,8 @@ public class CursomcApplication implements CommandLineRunner{
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
-	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -115,6 +118,21 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1,ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1,ip2,ip3));
+			
+		
 	}
 
 }
--- src/main/java/com/jorgefeitosa/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/jorgefeitosa/cursomc/domain/PagamentoComBoleto.java
@@ -1,6 +1,6 @@
 package com.jorgefeitosa.cursomc.domain;
 
-import java.io.Serializable;
+
 import java.util.Date;
 
 import javax.persistence.Entity;
